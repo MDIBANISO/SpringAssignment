236a02a498fa6ff7249e1cac5698f513cfd36012
LUVO <202250203@ufh.ac.za>
2025-04-12 16:11:10 +0200

    Assignment Group 10

---
+++ src/main/java/com/SpringAssigment/SpringAssigment/SpringAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAssigment.SpringAssigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssigmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringAssigment/SpringAssigment/SpringAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAssigment.SpringAssigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
